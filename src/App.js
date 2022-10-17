f9dfb785ce50ab2f7c360ae47564159dfe8b507e
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-17 22:49:17 +0600

    Navigation created with React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home'
-
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import New from './pages/new/New';
+import Single from './pages/single/Single'
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='users'>
+              <Route index element={<List/>} />
+              <Route path=':userId' element={<Single/>} />
+              <Route path='new' element={<New/>} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
