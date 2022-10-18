e45f2f95d0a599d22b7b882846479cf667b42daf
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-18 11:45:58 +0600

    navbar designed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./home.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 export default function Home() {
   return (
     <div className='home'>
       <Sidebar/>
       <div className="homeContainer">
+        <Navbar/>
         Container
       </div>
     </div>
