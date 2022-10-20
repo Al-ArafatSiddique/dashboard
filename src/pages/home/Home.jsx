196b261b14758a0ec3deec20c255a4840120dc2b
Al Arafat Siddique <arafatdipto@gmail.com>
2022-10-20 12:08:01 +0600

    material ui basic table imported

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Widget from '../../widget/Widget';
 import Featured from '../../components/featured/Featured';
 import Chart from '../../components/chart/Chart';
+import List from '../../components/table/List';
 export default function Home() {
   return (
     <div className='home'>
@@ -22,6 +23,11 @@ export default function Home() {
         <div className="charts">
           <Featured/>
           <Chart/>
+        </div>
+
+        <div className="listContainer">
+          <div className="listTitle">Last Transactions</div>
+          <List/>
         </div>
 
       </div>
